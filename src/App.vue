ab9710f13d38f0cd452b4eca08a946771dc420fb
Julian <dreerr@gmail.com>
2022-10-16 00:58:02 +0200

    cleanup

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,7 @@ import MMPSearch from "./components/MMPSearch.vue";
 </script>
 
 <template>
-  <div class="wrapper" id="single-wrapper">
-    <div class="container" id="content" tabindex="-1">
-      <div class="row">
-        <div class="col-md-12 content-area" id="primary">
-          <main class="site-main" id="main">
-            <MMPSearch />
-          </main>
-        </div>
-      </div>
-    </div>
-  </div>
+  <MMPSearch />
 </template>
 
-<style scoped>
-.foo {
-  margin: 0;
-}
-</style>
+<style scoped></style>
