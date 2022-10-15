a73bbc4d5b97e455b80d83da32a50a30b1f43d6c
Julian <dreerr@gmail.com>
2022-10-15 23:52:43 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# ACDH-CH Frontend Web Developer Assessment Task
+
+In this repository you can find my submission for the assessment task.
+
+## Notes on sorting
+
+I implemented a very preliminary sorting algorithm, which counts the occurences of the search query. This is only done within the current page, this is far from being a proper solution.
+
+## Notes on the quality of the API
+
+One of the things which I found a bit inconsistent is the mixed labeling of german and english terms. Furthermore some data seems to be duplicated (e.g. `results[].start_date` and `results[].text.start_date`).
---
+++ src/components/MMPSearch.vue
@@ -0,0 +1,192 @@
+<script setup lang="ts">
+import { computed, ref, Ref } from "vue";
+import { BPagination, BFormCheckbox } from "bootstrap-vue-3";
+import ky from "ky";
+
+interface MMPResult {
+  count: number;
+  next?: string;
+  previous?: string;
+  results: Array<any>;
+}
+
+let query: string = "";
+const apiEndpoint = "https://mmp.acdh-dev.oeaw.ac.at/api/stelle/";
+const queryField = ref("");
+const page = ref(1);
+const pageSize = ref(20);
+const isLoading = ref(false);
+const experimentalSorting = ref(false);
+
+const data: Ref<MMPResult> = ref({ count: 0, results: [] });
+
+const loadData = async () => {
+  if (queryField.value === "") return;
+  if (queryField.value !== query) {
+    query = queryField.value;
+    page.value = 1;
+  }
+
+  isLoading.value = true;
+  const params = new URLSearchParams({
+    zitat: query,
+    zitat_lookup: "icontains",
+    limit: pageSize.value.toString(),
+    offset: ((page.value - 1) * pageSize.value).toString(),
+  });
+
+  try {
+    data.value = await ky(apiEndpoint + "?" + params).json();
+    console.debug(data.value);
+  } catch (error) {
+    alert("error fetching data: " + error);
+  }
+  if (data.value.count === 0) {
+    alert("no results");
+  }
+  isLoading.value = false;
+};
+
+const loadPage = async (pageNum: number) => {
+  page.value = pageNum;
+  loadData();
+};
+
+const paginationUpdated = async () => {
+  await loadData();
+  window.scrollTo({ left: 0, top: 0, behavior: "smooth" });
+};
+
+const sortedResults = computed(() => {
+  if (experimentalSorting.value) {
+    const countQuery = (obj: any) => {
+      var regEx = new RegExp(query, "ig");
+      return (JSON.stringify(obj).match(regEx) || []).length;
+    };
+    return data.value.results
+      .slice()
+      .sort((a: any, b: any) => countQuery(b) - countQuery(a));
+  }
+  return data.value.results;
+});
+
+const highlightQuery = (text: string) => {
+  var regEx = new RegExp(query, "ig");
+  return text.replace(regEx, '<span class="highlight">$&</span>');
+};
+
+const getItemURI = (item: any) => {
+  // I felt better extracting ID from url and not to use legacy_pk+1
+  const id: string = item.url.match(/\d+/)[0];
+  return "https://mmp.acdh-dev.oeaw.ac.at/archiv/stelle/detail/" + id;
+};
+
+const getKeywords = (item: any) =>
+  item.key_word.map((e: any) => e.stichwort).join(", ");
+
+const getAuthors = (item: any) =>
+  item.text.autor.map((e: any) => e.name).join(", ");
+</script>
+
+<template>
+  <article>
+    <header class="entry-header">
+      <h1 class="entry-title">MMP</h1>
+    </header>
+    <div class="entry-content">
+      <div class="d-flex">
+        <div class="form-group">
+          <input
+            class="form-control ds-input"
+            v-model="queryField"
+            @keyup.enter="loadData()"
+            placeholder="Suche..."
+            spellcheck="false"
+          />
+        </div>
+        <div class="form-group ml-4">
+          <select
+            v-model.number="pageSize"
+            @change="loadPage(1)"
+            class="form-control"
+          >
+            <option>5</option>
+            <option>10</option>
+            <option>20</option>
+            <option>50</option>
+            <option>100</option>
+          </select>
+        </div>
+        <div class="form-group ml-5 pt-2">
+          <b-form-checkbox v-model="experimentalSorting"
+            >Experimentelles Sortieren</b-form-checkbox
+          >
+        </div>
+        <div class="ml-auto pt-2">
+          <span v-if="isLoading">Laden...</span>
+          <span v-else-if="data.count > 0">Ergebnisse: {{ data.count }}</span>
+        </div>
+      </div>
+
+      <table
+        class="table table-striped"
+        v-if="data.count > 0"
+        :class="{ loading: isLoading }"
+      >
+        <thead>
+          <tr>
+            <th>Label</th>
+            <th>Stichworte</th>
+            <th>Autor:innen</th>
+            <th>Jahresspanne</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in sortedResults" v-bind:key="item">
+            <td>
+              <a :href="getItemURI(item)" target="_blank"
+                ><span
+                  class="highlighted-text"
+                  v-html="highlightQuery(item.display_label)"
+                ></span
+              ></a>
+            </td>
+            <td>{{ getKeywords(item) }}</td>
+            <td>{{ getAuthors(item) }}</td>
+            <td>
+              {{
+                item.text.start_date +
+                (item.text.start_date === "" || item.text.end_date === ""
+                  ? ""
+                  : " – ") +
+                item.text.end_date
+              }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <b-pagination
+        v-if="data.count / pageSize > 1"
+        class="justify-content-center"
+        v-model="page"
+        :total-rows="data.count"
+        :per-page="pageSize"
+        :limit="7"
+        :disabled="isLoading"
+        @update:model-value="paginationUpdated()"
+      ></b-pagination>
+    </div>
+  </article>
+</template>
+
+<style scoped>
+.loading {
+  opacity: 0.5;
+}
+a:hover {
+  filter: none !important;
+}
+.highlighted-text :deep(.highlight) {
+  background-color: rgb(242, 255, 156) !important;
+}
+</style>
